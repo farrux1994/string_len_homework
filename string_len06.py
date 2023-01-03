7489755df0101ad161e7fb0da42060a97eedb85a
farrux1994 <f.turdiyevbek94@gmail.com>
2023-01-03 19:53:15 +0500

    string_len06

--- string_len06.py
+++ string_len06.py
@@ -7,7 +7,7 @@ def main(s1,s2):
     Returns:
         shortest string
     """
-    if len(s1) < len(s2):
+    if len(str(s1)) < len(str(s2)):
         return s1
 y = main('code', 'python')
 print(y)
