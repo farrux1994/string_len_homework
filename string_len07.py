e849d0af5c52bc2498016c584a3886b795293ea9
farrux1994 <f.turdiyevbek94@gmail.com>
2023-01-03 20:16:23 +0500

    string_len07

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,29 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    satr = ""
+   
+    len_1 = len(s1)
+    len_2 = len(s2)
+    len_3 = len(s3)
+
+    if len_1 % 2 != 0:
+        if len(satr) == 0:
+            satr += s1
+
+    if len_2 % 2 != 0:
+        if len(satr) == 0:
+            satr += s2
+        else:
+            satr += ", " + s2
+
+    if len_3 % 2 != 0:
+        if len(satr) == 0:
+            satr += s3
+        else:
+            satr += ", " + s3
+
+    return f"[{satr}]"
+
+y = main("code", "python", "coder")
+print(y)
